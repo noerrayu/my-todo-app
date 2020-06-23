87ec43d588353a3743fa4a17bd5ca1731cd0493c
Noer Ayu <ayu.intan117@gmail.com>
2020-06-23 09:26:49 +0700

    Updates

---
+++ precache-manifest.fe20d6e838ef2b77b1c3fb6edc39dbc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a4a9f5bbc8f774bf36146ba6d6badf",
+    "url": "/my-todo-app/index.html"
+  },
+  {
+    "revision": "3c69d3e91cfa84858593",
+    "url": "/my-todo-app/static/css/main.5576f456.chunk.css"
+  },
+  {
+    "revision": "c982703e73d992027072",
+    "url": "/my-todo-app/static/js/2.5001482f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-todo-app/static/js/2.5001482f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c69d3e91cfa84858593",
+    "url": "/my-todo-app/static/js/main.a669cf5d.chunk.js"
+  },
+  {
+    "revision": "7368ecffb913127e3944",
+    "url": "/my-todo-app/static/js/runtime-main.e4b067ed.js"
+  }
+]);
